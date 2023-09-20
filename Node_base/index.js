a63e90c69bf677e8666a9a4d254f24bc9fa2c36d
milenaRuizPiccinini <129749751+milenaRuizPiccinini@users.noreply.github.com>
2023-09-20 08:53:58 -0300

    explicacionWebSocket

--- Node_base/index.js
+++ Node_base/index.js
@@ -1,14 +1,4 @@
 
-/*  Paquetes instalados: -g nodemon, express, express-handlebars, body-parser, mysql2
-    Agregado al archivo "package.json" la línea --> "start": "nodemon index"
-    
-    Proyecto "Node_base"
-    Desarrollo de Aplicaciones Informáticas - 5to Informática
-    
-    Docentes: Nicolás Facón, Martín Rivas
-    
-    Revisión 1 - Año 2021
-*/
 //Cargo librerías instaladas y necesarias
 const express = require('express'); //Para el manejo del servidor Web
 const exphbs  = require('express-handlebars'); //Para el manejo de los HTML
@@ -27,9 +17,23 @@ app.set('view engine', 'handlebars'); //Inicializo Handlebars
 
 const Listen_Port = 3000; //Puerto por el que estoy ejecutando la página Web
 
-app.listen(Listen_Port, function() {
+const server=app.listen(Listen_Port, function() {
     console.log('Servidor NodeJS corriendo en http://localhost:' + Listen_Port + '/');
 });
+
+const io= require('socket.io')(server);
+
+/*const sessionNiddleware=session({
+    secret: 'sararasthastka',
+    resave: true,
+    saveUnintialized: false,
+});
+
+app.use(sessionMiddleware);
+
+io.use(function(socket, next) {
+    sessionMiddleware(socket.request, socket.request.res, next);
+});*/
 
 /*
     A PARTIR DE ESTE PUNTO GENERAREMOS NUESTRO CÓDIGO (PARA RECIBIR PETICIONES, MANEJO DB, ETC.)
@@ -78,3 +82,16 @@ app.delete('/login', function(req, res) {
     console.log("Soy un pedido DELETE", req.body); //En req.body vamos a obtener el objeto con los parámetros enviados desde el frontend por método DELETE
     res.send(null);
 });
+
+io.on("connection", (socket) => {
+    //Esta línea es para compatibilizar con lo que venimos escribiendo
+    const req = socket.request;
+
+    //Esto serìa el equivalente a un app.post, app.get...
+    socket.on('incoming-message', data => {
+        console.log("INCOMING MESSAGE:", data);
+        io.edmit("server-message", {mensaje:"MENSAJE DE SERVIDOR"})
+    });
+});
+
+setInterval(() => io.emit("server-message", { mensaje: "MENSAJE DEL SERVIDOR" }), 2000);
